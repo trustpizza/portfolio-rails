ba664394e9744449a8259adeaedcebde79bacb41
trustpizza <maxelolsson@gmail.com>
2023-04-03 13:21:08 -0500

    Create dorpdown menu

---
+++ esbuild.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+
+require("esbuild").build({
+  entryPoints: ["application.js"],
+  bundle: true,
+  outdir: path.join(process.cwd(), "app/assets/builds"),
+  absWorkingDir: path.join(process.cwd(), "app/javascript"),
+  // watch: process.argv.includes("--watch"),
+  loader: {
+    '.js': 'jsx',
+    '.png': 'file',
+    '.svg': 'file'
+  },
+}).catch(() => process.exit(1))
