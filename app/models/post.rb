54890932c7bed172bab86bba9005d0b652d73d3c
trustpizza <maxelolsson@gmail.com>
2023-04-04 13:44:53 -0500

    rake assets:precompile makes tailwind work again

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ApplicationRecord
     validates :title, presence: true
     has_rich_text :content
 
-    has_one_attached :thumbnail
+    has_one_attached :thumbnail do |attachable|
+        attachable.variant :thumb, resize_to_limit: [100, 100]
+      end
 end
