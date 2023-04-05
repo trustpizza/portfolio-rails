016f050e3565eb43c7794567204678f7476ac509
trustpizza <maxelolsson@gmail.com>
2023-04-05 10:47:44 -0500

    create similar view from posts for projects

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_02_200810) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_05_153817) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -57,6 +57,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_02_200810) do
     t.datetime "updated_at", null: false
     t.text "title"
     t.text "description"
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string "link"
+    t.text "description"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
