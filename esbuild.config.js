14e3742931885f310c9389d12293ea08f1e07913
trustpizza <maxelolsson@gmail.com>
2023-04-04 22:42:01 -0500

    Find a config file that works (kinda)

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,14 +1,44 @@
 const path = require('path')
 
-require("esbuild").build({
+require("esbuild").context({
   entryPoints: ["application.js"],
   bundle: true,
+  sourcemap: true,
+  publicPath: 'assets',
   outdir: path.join(process.cwd(), "app/assets/builds"),
   absWorkingDir: path.join(process.cwd(), "app/javascript"),
-  // watch: process.argv.includes("--watch"),
   loader: {
-    '.js': 'jsx',
-    '.png': 'file',
-    '.svg': 'file'
-  },
+        '.js': 'jsx',
+        '.png': 'file',
+        '.svg': 'file'
+      },
+  plugins: [],
+  minify: process.argv.includes("--minify")
+}).then(context => {
+  if (process.argv.includes("--watch")) {
+    // Enable watch mode
+    context.watch()
+  } else {
+    // Build once and exit if not in watch mode
+    context.rebuild().then(result => {
+      context.dispose()
+    })
+  }
 }).catch(() => process.exit(1))
+// const path = require('path');
+
+// require("esbuild").build({
+//   entryPoints: ["application.js"],
+//   bundle: true,
+//   outdir: path.join(process.cwd(), "app/assets/builds"),
+//   absWorkingDir: path.join(process.cwd(), "app/javascript"),
+//   loader: {
+//     '.js': 'jsx',
+//     '.png': 'file',
+//     '.svg': 'file'
+//   },
+//   // custom plugins will be inserted is this array
+//   plugins: [],
+// }).catch(() => process.exit(1));
+
+// esbuild app/javascript/*.* --bundle --sourcemap --outdir=app/assets/builds --public-path=assets
