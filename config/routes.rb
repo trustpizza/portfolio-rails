3f5e72786dc455d36e6f71da60607d7774ec96a5
trustpizza <maxelolsson@gmail.com>
2023-04-11 19:42:46 -0500

    Add custom path for about

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :projects
   root 'home#home'
+  get "/about", to: "home#about"
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
