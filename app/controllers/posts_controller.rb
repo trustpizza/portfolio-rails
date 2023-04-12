1a9f6ff78db1175d74ab9c96b99471c2a78359b4
trustpizza <maxelolsson@gmail.com>
2023-04-11 19:37:50 -0500

    Add basic authentication

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
-
+  http_basic_authenticate_with name: "admin", password:"123", only: %i[edit update destroy new]
   # GET /posts or /posts.json
   def index
     @posts = Post.all
